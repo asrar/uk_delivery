9c805bd997c1900ce762ee34e856b35a023a52aa
Asrar <asrarulhasan@gmail.com>
2022-09-12 15:46:00 +0500

    Initial commit

---
+++ lib/theme/light_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData light = ThemeData(
+  fontFamily: 'Roboto',
+  primaryColor: Color(0xFFf47216),
+  secondaryHeaderColor: Color(0xFF009f67),
+  disabledColor: Color(0xffa2a7ad),
+  //backgroundColor: Color(0xFF343636),
+  errorColor: Color(0xFFdd3135),
+  brightness: Brightness.dark,
+  hintColor: Color(0xFFbebebe),
+  cardColor: Colors.black,
+  colorScheme: ColorScheme.dark(primary: Color(0xFFf47216), secondary: Color(0xFFf47216)),
+  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(primary: Color(0xFFf47216))),
+);
